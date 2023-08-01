bd23d58175e6ee6f16b3e02c1a9bcd1fdafb77ef
ghanshyam <bca1902113@gmail.com>
2023-08-01 11:34:04 +0530

    first

---
+++ app/Http/Controllers/data.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\client;
+use App\Models\student;
+
+class data extends Controller
+{
+    public function data(){
+
+        $table1 =  client::all();
+        $table2 =  student::all();
+
+        $merge = [
+            'data_table1' => $table1,
+            'data_table2' => $table2,
+        ];
+
+        return response()->json($merge);
+    }
+}
