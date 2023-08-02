4d8f4fa4bb3ba10d5798c1d47ebcc7a8940b4fd9
ghanshyam <bca1902113@gmail.com>
2023-08-02 12:04:46 +0530

    all

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StripePaymentController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/stripe', [StripePaymentController::class, 'stripe']);
-Route::post('/stripe', [StripePaymentController::class, 'stripePost'])->name('stripe.post');
